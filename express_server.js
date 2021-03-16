047e15bb5447f2387216bdf674729c923960cfa4
Sarah <sarah.pflug@shopify.com>
2021-03-16 01:01:13 +0000

    added redirect for short URLS and did some variable name clena up

--- express_server.js
+++ express_server.js
@@ -18,13 +18,21 @@ const bodyParser = require("body-parser");  // will convert the request body int
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  // const longUrl = req.body.longURL
+  const newShortUrl = generateRandomString()   /// runs our function which will become the shortUrl
+  urlDatabase[newShortUrl] = req.body.longURL
+  res.redirect(`/urls/${newShortUrl}`);         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
